c4f901381bb764d4424f1fdcd9604f14c49d5bb9
Lukas Werling <lukas.werling@gmail.com>
2016-07-04 17:52:44 +0200

    Add Lib_ConveyorTarget

---
+++ Blocks.ocd/Libs.ocd/Lib_ConveyorTarget.ocd/Script.c
@@ -0,0 +1,54 @@
+/**
+	Lib_ConveyorTarget
+	
+	Manages the connection to the conveyor network. A target can only be
+	connected to one network at once.
+
+	@author Luchs
+*/
+
+local conveyor_network;
+
+local IsConveyorTarget = true;
+public func IsConnectedToConveyor() { return conveyor_network != nil; }
+
+private func FindConveyor(int range)
+{
+	return FindObject(Find_ID(Tile_Conveyor), Find_OnLine(0, 0, 0, -range));
+}
+
+// Discover and updates a connected conveyor network.
+protected func DiscoverConveyor()
+{
+	// TODO: Proper constant for the collection range
+	var conveyor = FindConveyor(40);
+	var network = conveyor && conveyor.network;
+	if (network != conveyor_network)
+	{
+		if (conveyor_network)
+			conveyor_network->RemoveTarget(this);
+		if (network)
+			network->AddTarget(this);
+		conveyor_network = network;
+	}
+}
+
+static const FxConveyorTarget = new Effect
+{
+	Timer = func()
+	{
+		Target->DiscoverConveyor();
+	},
+
+	Destruction = func()
+	{
+		if (Target.conveyor_network)
+			Target.conveyor_network->RemoveTarget(Target);
+	},
+};
+
+func Initialize()
+{
+	CreateEffect(FxConveyorTarget, 1, 30);
+	return _inherited(...);
+}
--- Blocks.ocd/Tiles.ocd/Tile_Conveyor.ocd/Network.ocd/Script.c
+++ Blocks.ocd/Tiles.ocd/Tile_Conveyor.ocd/Network.ocd/Script.c
@@ -9,12 +9,16 @@
 // Root node of the conveyor network.
 local root;
 
+// List of targets, keyed on object number for fast manipulation.
+local targets; 
+
 public func Create(object root)
 {
 	if (this != ConveyorNetwork) FatalError("id call only");
 
 	var network = CreateObject(ConveyorNetwork);
 	network.root = root;
+	network.targets = [];
 
 	return network;
 }
@@ -25,8 +29,9 @@ public func Merge(object other)
 	if (other == this) return false; // already the same network
 
 	other.root->SetNetwork(this, other);
-
 	other->RemoveObject();
+
+	UpdateTargets();
 	return true;
 }
 
@@ -39,7 +44,29 @@ public func Split(object node)
 	var new_network = ConveyorNetwork->Create(node);
 	node->SetNetwork(new_network, this);
 
+	UpdateTargets();
+
 	return true;
+}
+
+public func AddTarget(object target)
+{
+	if (GetIndexOf(targets, target) == -1)
+		PushBack(targets, target);
+}
+
+public func RemoveTarget(object target)
+{
+	RemoveArrayValue(targets, target, true /* unstable */);
+}
+
+// Makes each registered target re-discover its network.
+private func UpdateTargets()
+{
+	for (var t in targets)
+	{
+		t->DiscoverConveyor();
+	}
 }
 
 func SaveScenarioObject(props)
